0892f301c77e6ed0ae839db27ada26f234b9c1bc
Jessie S <jessie.sholler@gmail.com>
2020-01-23 16:37:38 -0500

    update

---
+++ Data/Models/Book.cs
@@ -0,0 +1,22 @@
+using System;
+namespace Reading_List.Controllers
+
+{
+    public class Book{
+        //Identifier 
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Description { get; set; }
+
+        public double? Rate { get; set; }
+
+        public DateTime? DateStart { get; set; }
+
+        public DateTime? DateEnd { get; set; }
+
+    }
+}
