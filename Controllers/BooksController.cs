0a734cf3797e4ed673b60c3e58e9a2a4b04b70c8
Jessie S <jessie.sholler@gmail.com>
2020-01-23 18:51:34 -0500

    bug fixed

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -7,13 +7,13 @@ namespace Reading_List.Controllers
     [Route("api/[controller]")]
     public class BooksController : Controller
     {
-        // Injecting Book Service in Controller 
+    //     // Injecting Book Service in Controller 
         private IBookService _service; 
         public BooksController(IBookService service)
         {
             _service = service; 
         }
-        //Create/Add a new Book
+    //     //Create/Add a new Book
         [HttpPost("AddBook")]
         public IActionResult AddBook([FromBody]Book book)
         {
@@ -21,7 +21,7 @@ namespace Reading_List.Controllers
             return Ok("Added");
         }
         // New Api Endpoint -> ReadAllBooks
-        [HttpGet("[action")]
+        [HttpGet("[action]")]
         // Define the implimentation 
         public IActionResult GetBooks(){
             var allBooks = _service.GetAllBooks(); 
